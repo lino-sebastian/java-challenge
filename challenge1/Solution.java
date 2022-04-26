90ac375291b103a02f75f41bfe821d8d1e96a6aa
linosebastian <talk2linojoy@gmail.com>
2022-04-26 16:12:47 +0530

    Java Challenge 1 Initial Upload

---
+++ challenge1/Solution.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static void plusMinus(List<Integer> arr) {
+        final int[] positiveCount ={0};
+        final int[] negativeCount ={0};
+        final int[] zeroCount ={0};
+        arr.forEach(integerValue->{
+            if(integerValue>0){
+                positiveCount[0]++;
+            }
+            else if(integerValue<0){
+                negativeCount[0]++;
+            }
+            else{
+                zeroCount[0]++;
+            }
+        });
+        System.out.printf("%.6f%n",(float)positiveCount[0]/arr.size());
+        System.out.printf("%.6f%n",(float)negativeCount[0]/arr.size());
+        System.out.printf("%.6f%n",(float)zeroCount[0]/arr.size());
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
